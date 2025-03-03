0ca5c1dc4ae3ae8226b88799813ca38aac320c05
CmexKota96 <legion9666@gmail.com>
2025-03-03 22:11:45 +0500

    1.1. Что такое import [#505278]

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -2,14 +2,17 @@ package ru.job4j.calculator;
 import static ru.job4j.math.MathFunction.*;
 
 public class MathCalculator {
+
     public static double sumAndMultiply(double first, double second) {
         return sum(first, second)
                 + multiply(first, second);
     }
+
     public static double sumDifAndDegree(double first, double second) {
         return dif(first, second)
                 + degree(first, second);
     }
+
     public static double sumAllFunction(double first, double second) {
         return sum(first, second) + multiply(first, second)
                 + dif(first, second) + degree(first, second);
@@ -20,5 +23,4 @@ public class MathCalculator {
         System.out.println("Результат расчета sumDifAndDegree равен: " + sumDifAndDegree(10, 20));
         System.out.println("Результат расчета sumAllFunction равен: " + sumAllFunction(10, 20));
     }
-
 }
