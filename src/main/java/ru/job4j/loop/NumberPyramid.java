f04872d8840802cd336feca04e4e1c97c4eec4fa
KlimovaAlina <alina_klimova1503@mail.ru>
2025-03-20 21:42:41 +0500

    Числовая пирамида [#505218]

--- src/main/java/ru/job4j/loop/NumberPyramid.java
+++ src/main/java/ru/job4j/loop/NumberPyramid.java
@@ -3,13 +3,13 @@ package ru.job4j.loop;
 public class NumberPyramid {
     public static void draw(int n) {
         for (int i = 1; i <= n; i++) {
-            for (int k = n - i ; k != 0; k-- ) {
+            for (int k = n - i; k != 0; k--) {
                 System.out.print(" ");
             }
             for (int j = 1; j <= i; j++) {
                 System.out.print(j);
             }
-            for (int j = i-1; j >= 1; j-- ) {
+            for (int j = i-1; j >= 1; j--) {
                 System.out.print(j);
             }
             System.out.println();
