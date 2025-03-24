0700be0a71688df0e5a413d36b45eaf74ec2de97
KlimovaAlina <alina_klimova1503@mail.ru>
2025-03-24 21:53:33 +0500

    Заполнение массива [#505222]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива surnames равен: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива prices равен: " + prices.length);
+        String [] names = new String[5];
+        names[0] = "Dog";
+        names[1] = "Cat";
+        names[2] = "Frog";
+        names[3] = "Snake";
+        names[4] = "Eagle";
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 }
