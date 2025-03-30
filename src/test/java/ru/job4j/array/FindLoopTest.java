e7f61be5e66899ad66629178553c6118a4e9a99d
CmexKota96 <legion9666@gmail.com>
2025-03-30 23:18:16 +0500

    Классический поиск перебором [#505227 #537910]

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -14,11 +14,20 @@ class FindLoopTest {
     }
 
     @Test
-    void whenArrayHasNot6ThenMinus1() {
-        int[] data = new int[] {5, 10, 3};
-        int element = 5;
+    void whenArrayHasNot7ThenMinus1() {
+        int[] data = new int[] {4, 2, 9};
+        int element = 7;
         int result = FindLoop.indexOf(data, element);
-        int expected = 0;
+        int expected = -1;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenArrayHas8Then4() {
+        int[] data = new int[] {4, 2, 9, 3, 8};
+        int element = 8;
+        int result = FindLoop.indexOf(data, element);
+        int expected = 4;
         assertThat(result).isEqualTo(expected);
     }
 }
