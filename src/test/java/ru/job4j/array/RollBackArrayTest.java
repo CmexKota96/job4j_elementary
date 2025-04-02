f8a355394892723f53f6092236a09c9e3fd4cdfb
CmexKota96 <legion9666@gmail.com>
2025-04-02 16:56:45 +0500

    Обход массива с последнего элемента [#505235]

--- src/test/java/ru/job4j/array/RollBackArrayTest.java
+++ src/test/java/ru/job4j/array/RollBackArrayTest.java
@@ -3,7 +3,6 @@ package ru.job4j.array;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
-
 class RollBackArrayTest {
     @Test
     public void whenEmpty() {
